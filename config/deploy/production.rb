1a6ffd6cc6e551c2dcf0aa9ac89537a86eb97ec7
fenet <fenetgucci@gmail.com>
2018-08-06 12:00:58 +0300

    production.rb edited second time

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,16 +49,16 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+  server "206.189.134.137",
+    user: "fena",
+    roles: %w{web app},
+    ssh_options: {
+      user: "fena", # overrides user setting above
+      keys: %w(/home/fena/.ssh/id_rsa),
+      forward_agent: true,
+      #auth_methods: %w(publickey password)
+      # password: "please use keys"
+    }
 
 #set :application, "HR"
 #set :rails_env, 'production'
