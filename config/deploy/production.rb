58b233fefcaae9415a865d56cdfd6e4b91591be9
fenet <fenetgucci@gmail.com>
2018-08-06 12:19:29 +0300

    production.rb edited second time

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,28 +49,28 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-  server "206.189.134.137",
-    user: "fena",
-    roles: %w{web app},
-    ssh_options: {
-      user: "fena", # overrides user setting above
-      keys: %w(/home/fena/.ssh/id_rsa),
-      forward_agent: true
-      #auth_methods: %w(publickey password)
-      # password: "please use keys"
-    }
+# server "example.com",
+#   user: "user_name",
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: "user_name", # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: "please use keys"
+#   }
 
-#set :application, "HR"
-#set :rails_env, 'production'
-#set :branch, "master"
-#
-#server '206.189.134.137', user: 'fena', roles: %w{web}, primary: true
-#set :deploy_to, "/var/www/apps/#{fetch(:application)}"
-#
-#
-#set :ssh_options, {
-#    forward_agent: true,
-#    auth_methods: %w(password),
-#    password: 'fenet1986',
-#    user: 'fena',
-#}
+set :application, "HR"
+set :rails_env, 'production'
+set :branch, "master"
+
+server '206.189.134.137', user: 'fena', roles: %w{web app db}, primary: true
+set :deploy_to, "/var/www/apps/#{fetch(:application)}"
+
+
+set :ssh_options, {
+    forward_agent: true,
+    #auth_methods: %w(password),
+    #password: 'pass',
+    user: 'fena',
+}
