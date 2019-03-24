dbb5c6b84dfd524bfc87c45c8cf59338caca7c3d
fenet <fenetgucci@gmail.com>
2019-03-24 14:17:37 +0300

    amharic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
+  
+# get 'feedbacks/index'
 
+# get 'feedbacks/new'
+
+# get 'feedbacks/edit'
+
+#  get 'feedbacks/delete'
+
+# get 'feedbacks/show'
+   get 'services/service_amh'
+#
+#  get 'cvs/index'
+#
+#  get 'cvs/edit'
+#
+#  get 'cvs/delete'
+#
+#  get 'cvs/new'
+#
+#  get 'cvs/show'
+#
   get 'ad_page/index'
 #
   get 'services/show_all'
@@ -11,12 +32,35 @@ get 'static_pages/operation'
 #
 get 'static_pages/current_operation'
 
+#  get 'contacts/delete'
 #
+#  get 'contacts/edit'
+#
+#  get 'contacts/index'
+
+# get 'contacts/show_com'
+
   get 'contacts/show'
-#
+
   get 'static_pages/faqs'
 
+  get 'static_pages/home_amh'
 
+  get 'static_pages/Amharic'
+
+
+# get 'cvs/show'
+#
+#  get 'vacancies/index'
+#
+#  get 'vacancies/edit'
+#
+#  get 'vacancies/new'
+#
+#  get 'vacancies/delete'
+#
+#  get 'vacancies/show' 
+  
   get 'cvs/cv_complet'
 
   get 'feedbacks/thank_you'
@@ -40,7 +84,7 @@ get 'static_pages/current_operation'
   resources :cvs, except: [:show] do
     member do
        get :delete , :download_file
-
+     
      end
   end
 
@@ -49,7 +93,7 @@ get 'static_pages/current_operation'
        get :delete
       end
   end
-
+ 
 
    resources :vacancies, except: [:show] do
     member do
@@ -60,25 +104,26 @@ get 'static_pages/current_operation'
 resources :contacts, except: [:show] do
     member do
        get :delete
-
+     
      end
   end
 
   resources :feedbacks, except: [:show] do
     member do
        get :delete
-
+     
      end
   end
   devise_for :admins
-
+  
  # devise_for :users
 
   root to: 'static_pages#home'
 
   get 'blogs/index'
 
- # uncomment this for page 404 //get "*path", to: redirect("/404.html")
+ # uncomment this for page 404 //
+ get "*path", to: redirect("/404.html")
 
 
 
